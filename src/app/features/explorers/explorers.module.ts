e361e7c4c95ecdb84a3c987ed8625fc64f3efa0b
ameen <belkhodja.amine@gmail.com>
2024-06-10 21:56:27 +0200

    feat: set project structure feat: create features modules

---
+++ src/app/features/explorers/explorers.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ExplorersRoutingModule } from './explorers-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ExplorersRoutingModule
+  ]
+})
+export class ExplorersModule { }
---
+++ src/app/features/explorers/explorers/explorers.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-explorers',
+  standalone: true,
+  imports: [],
+  templateUrl: './explorers.component.html',
+  styleUrl: './explorers.component.scss'
+})
+export class ExplorersComponent {
+
+}
