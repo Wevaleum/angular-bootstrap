7a9067bd43a4847b047529750a40d6a59b9ebd6e
ameen <belkhodja.amine@gmail.com>
2024-06-11 21:57:55 +0200

    conf: set editors configuration

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.example.app',
   appName: 'angular-bootstrap',
-  webDir: 'dist'
+  webDir: 'dist/angular-bootstrap/browser'
 };
 
 export default config;
